ac18501d97cda1d346b16f6ef9dc5ecbdaf1b888
m-jozefowicz <mjozefowicz.wso@gmail.com>
2017-05-26 16:30:01 +0200

    Finshed Skills sections ~MJ

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function(){
-  
+
+  kissuiScrollAnim.setOptions({
+  'autoReset': false,
+  // 'triggerOnInit': true
+  })
+
+	$(document).on('click', 'a', function(event){
+	    event.preventDefault();
+	    $('body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 800);
+	});
+
 });
